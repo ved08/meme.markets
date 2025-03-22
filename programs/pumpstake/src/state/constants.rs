43bd153ac6862f9aa08869f9bf1625a759efdb28
ved08 <vedvardhan.g@gmail.com>
2025-03-22 18:19:14 +0530

    added constants

--- programs/pumpstake/src/state/constants.rs
+++ programs/pumpstake/src/state/constants.rs
@@ -2,3 +2,12 @@ use anchor_lang::constant;
 
 #[constant]
 pub const MAX_OPTIONS: u8 = 8;
+
+#[constant]
+pub const CP_RATIO: u64 = 2_000_000_000_000;
+#[constant]
+pub const INITIAL_SOL_CAP: u64 = 2000;
+#[constant]
+pub const BILLION: u64 = 1_000_000_000;
+#[constant]
+pub const MAX_AMM_ERROR: f64 = 15.0;
